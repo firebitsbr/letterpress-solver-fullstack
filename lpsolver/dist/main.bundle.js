529e89fe8f3b745f3f81197e9b1554911616a294
Samuel <qichen.d@outlook.com>
2018-08-09 03:14:12 +1200

    change host

--- lpsolver/dist/main.bundle.js
+++ lpsolver/dist/main.bundle.js
@@ -382,47 +382,17 @@ var MatchComponent = /** @class */ (function () {
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 var env = {
     production: false,
-    host: "192.168.1.69",
+    // host:"192.168.1.69",
     // host:"192.168.1.74",
     // host:"130.217.184.127",
-    // host:"localhost",
+    host: "localhost",
     port: "8080",
     player: [
         {
             id: '4WUD4X8522HSRM6X',
-            name: 'Samuell',
-            Host: 'https://solebonapi.com:443',
-            header: {
-                'accept': '*/*',
-                'accept-language': 'en-us',
-                'accept-encoding': 'gzip, deflate',
-                'connection': 'keep-alive',
-                'authorization': 'Basic NFdVRDRYODUyMkhTUk02WDphTDY3UGJJRVJD',
-                'x-solebonapi-token': 'ef5869e1954712dde93ed7bf8e865970e7b2b03374ed8c27365b1e3ff05513b9',
-                'user-agent': 'Letterpress/248 CFNetwork/811.5.4 Darwin/16.7.0'
-            },
-            GET: {
-                matchlist: '/api/1.0/lplist_matches.json?appkey=VI2ET0P4&offset=0&count=50',
-                checkword: '/api/1.0/lp_check_word.json?appkey=VI2ET0P4&word=',
-            }
         },
         {
             id: 'MMSJUKH9SYLQVZDY',
-            name: "semiconductor",
-            Host: 'https://solebonapi.com:443',
-            header: {
-                'accept': '*/*',
-                'accept-language': 'en-us',
-                'accept-encoding': 'gzip, deflate',
-                'connection': 'keep-alive',
-                'authorization': 'Basic TU1TSlVLSDlTWUxRVlpEWTpFSTBDOUtiSkQw',
-                'x-solebonapi-token': '35096253fa358ae5ac6d377c6680f1d974a9533c090580b9e648e6445f624c87',
-                'user-agent': 'Letterpress/248 CFNetwork/811.5.4 Darwin/16.7.0'
-            },
-            GET: {
-                matchlist: '/api/1.0/lplist_matches.json?appkey=VI2ET0P4&offset=0&count=50',
-                checkword: '/api/1.0/lp_check_word.json?appkey=VI2ET0P4&word=',
-            }
         },
     ]
 };
